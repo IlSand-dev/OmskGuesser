19e405110b3190e7b582241e38bd981d56833325
ilyaCoolDev <ilya@d3en.ru>
2021-11-06 17:19:52 +0600

    app is now only portrait

---
+++ app/src/main/java/ru/fcfts/omskguesser/InstructionActivity.java
@@ -0,0 +1,14 @@
+package ru.fcfts.omskguesser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InstructionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instruction);
+    }
+}
